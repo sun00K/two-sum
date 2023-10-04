2388f3c476459f84446444edb1373c7d6b38d98d
Sunoo Kim <ssuno0ki3@gmail.com>
2023-10-04 23:03:27 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    num_dict = {}
+    
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], i]
+        num_dict[num] = i
+    
+    return []
